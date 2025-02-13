df24da04cce9c982f88a1616670cebcec8c8c16e
araitatsuya-code <rd0801577@gmail.com>
2025-02-13 22:23:35 +0900

    feat: ESLint設定を更新し、TypeScriptおよびReactのプラグインを追加

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,34 @@
 module.exports = {
-  extends: ["universe/native"],
+  root: true,
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint", "react", "react-hooks"],
   rules: {
-    "prettier/prettier": "error",
-    "@typescript-eslint/no-unused-vars": "error",
+    "react/react-in-jsx-scope": "off",
+    "@typescript-eslint/no-unused-vars": ["error"],
     "react-hooks/rules-of-hooks": "error",
     "react-hooks/exhaustive-deps": "warn",
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+    },
   },
 };
