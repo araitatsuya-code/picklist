3b1f91e66e698812129f2fe89365e0ad5da02053
araitatsuya-code <rd0801577@gmail.com>
2025-04-07 00:05:20 +0900

    feat: PaperProviderにSlotコンポーネントを追加し、テーマを適用

--- App.tsx
+++ App.tsx
@@ -1,10 +1,13 @@
 import { Provider as PaperProvider } from 'react-native-paper';
 import { ThemeProvider } from './src/components/ThemeProvider';
+import { Slot } from 'expo-router';
 
 export default function App() {
   return (
     <ThemeProvider>
-      <PaperProvider></PaperProvider>
+      <PaperProvider theme={{}}>
+        <Slot />
+      </PaperProvider>
     </ThemeProvider>
   );
 }
